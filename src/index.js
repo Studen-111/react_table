30dcb0e41592c3314d3c44a3bac38ca7a5cf1965
Studen-111 <p61swmgisz7a@mail.ru>
2021-08-05 20:24:52 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
   import React from 'react'
   import ReactDOM from 'react-dom'
   import App from './App'
+  //import App from './Api' /* Pulling in API Data */
   import './index.css'
 
   ReactDOM.render(<App />, document.getElementById('root'))
